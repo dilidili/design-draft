1cb7837ae7e1065173168f55a9e1fe16e964caff
Wensheng Xu <xws@superid.cn>
2019-04-02 18:02:25 +0800

    feat reorganize sibling elements.

--- src/boundingRect2code.js
+++ src/boundingRect2code.js
@@ -9,6 +9,8 @@ let boundingRect = require('../assets/boundingRect');
 boundingRect = boundingRect.children.find(v => v.children && v.children.length > 0);
 
 function getCenterPoint(element) {
+  if (element._centerPoint) return element._centerPoint;
+
   const {
     rect: {
       x,
@@ -17,8 +19,9 @@ function getCenterPoint(element) {
       height,
     }
   } = element;
+  element._centerPoint = [x + width / 2, y + height / 2];
 
-  return [x + width / 2, y + height / 2];
+  return element._centerPoint;
 }
 
 function getFlexDirection(element) {
@@ -39,20 +42,75 @@ function computeSizeStyle(element) {
   element.style.height = element.rect.height;
 }
 
-function filterNoisy(element) {
+function createNodeWithChildren(children) {
+  const ret = {
+    rect: {
+      x: Infinity,
+      y: Infinity,
+      width: 0,
+      height: 0,
+    },
+    children,
+  };
+
+  children.forEach(child => {
+    ret.rect.x = Math.min(child.rect.x, ret.rect.x);
+    ret.rect.y = Math.min(child.rect.y, ret.rect.y);
+    ret.rect.width = Math.max(child.rect.width, ret.rect.width);
+    ret.rect.height = Math.max(child.rect.height, ret.rect.height);
+  });
+
+  return ret;
+}
+
+function filterNoisy(element, parentElement) {
   const {
     rect: {
       width,
       height,
     }
   } = element;
+  const {
+    rect: {
+      width: parentWidth,
+      height: parentHeight,
+    }
+  } = parentElement;
 
   let ret = true
-  ret = ret && Math.max(width, height) / Math.min(width, height) < 10  // 长宽比异常
-  return ret
+  ret = ret && Math.max(width, height) / Math.min(width, height) < 10; // radio abnormality.
+  // ret = ret && ((width * height) / (parentWidth * parentHeight)) > 0.0006; // area abnormality.
+  return ret;
+}
+
+const REORGNAIZE_ERROR = 5;
+function reorganizeChildren(element) {
+  element.children = element.children || [];
+  const dirtyArray = element.children.slice(element.children);
+
+  while(dirtyArray && dirtyArray.length) {
+    const current = dirtyArray.shift();
+    let newNode;
+
+    const verticalSiblings = element.children.filter(child => child !== current && Math.abs(getCenterPoint(current)[0] - getCenterPoint(child)[0]) < REORGNAIZE_ERROR);
+    if (verticalSiblings && verticalSiblings.length > 0) {
+      newNode = createNodeWithChildren([current, ...verticalSiblings]);
+    } else {
+      const horizontalSiblings = element.children.filter(child => child !== current && Math.abs(getCenterPoint(current)[1] - getCenterPoint(child)[1]) < REORGNAIZE_ERROR);
+      newNode = createNodeWithChildren([current, ...horizontalSiblings]);
+    }
+
+    if (newNode) {
+      element.children = element.children.filter(v => !newNode.children.find(w => w === v));
+      element.children.push(newNode);
+    }
+  }
 }
 
 function wrapWithStyle(element, depth = 0, maxDepth) {
+  // reorganize children.
+  reorganizeChildren(element);
+
   element.style = element.style || { backgroundColor: randomColor() };
 
   // necessary style.
@@ -63,7 +121,8 @@ function wrapWithStyle(element, depth = 0, maxDepth) {
   computeSizeStyle(element);
 
   if (!maxDepth || depth < maxDepth) {
-    (element.children || []).filter(filterNoisy).forEach((child) => wrapWithStyle(child, depth + 1, maxDepth));
+    element.children = (element.children || []).filter(child => filterNoisy(child, element))
+    element.children.forEach((child) => wrapWithStyle(child, depth + 1, maxDepth));
   } else {
     element.children = []
   }
